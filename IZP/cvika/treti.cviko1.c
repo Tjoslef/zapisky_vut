5567696c1c62ce844334d37626d2426aa1cacf4b
Tjoslef <josef.pasek17@gmail.com>
2024-10-03 11:49:12 +0200

    jede to

--- IZP/cvika/treti.cviko1.c
+++ IZP/cvika/treti.cviko1.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 int main(int argc,char* argv[10]){
+    if (argc < 3) {
+        printf("Chyba: Nedostatek argumentů. Zadejte příkaz a argument.\n");
+        return 1;
+    }
 char *prikaz = argv[1];
 char argument_2[11];
 strncpy(argument_2, argv[2], 10);
@@ -12,29 +15,44 @@ char druhy_Pr[] = "notalnum";
 char treti_Pr[] = "palindrom";
 char pouzeZnak[100];
 int index = 0;
+int jetoPal = 0;
 if(strcmp(prikaz,prvni_Pr ) == 0){
-    for(int i = 0; i < 10;i++){
+    for(int i = 0; argument_2[i] != '\0';i++){
         if(argument_2[i] >='A' &&  argument_2[i] <= 'Z'){
             argument_2[i] = argument_2[i] + 32;
             }
 
         }
-
-    }
-    else if(strcmp(prikaz, druhy_Pr) == 0){
-    for(int i = 0; i< 10; i++){
-       if(!(isdigit(argument_2[i]|| isalpha(argument_2[i])))){
-        pouzeZnak[index] = argument_2[i];
-        index++;
+printf("%s novy male pismena\n",argument_2);
+    }else if (strcmp(prikaz, druhy_Pr) == 0) {
+        for (int i = 0; argument_2[i] != '\0'; i++) {
+            // Opravená podmínka
+            if (!(isdigit(argument_2[i]) || isalpha(argument_2[i]))) {
+                pouzeZnak[index] = argument_2[i];
+                index++;
+            }
         }
+        pouzeZnak[index] = '\0';  // Ukončit řetězec
+        printf("%s - pouze nealfanumerické znaky\n", pouzeZnak);
+    }else if (strcmp(prikaz, treti_Pr) == 0) {
+        int prvni_index = 0;
+        int posledni_index = strlen(argument_2) - 1;
+        while(prvni_index < posledni_index){
+        if(!(argument_2[prvni_index] == argument_2[posledni_index])){
+            jetoPal = 1;
+            printf("%s neni palindrom\n",argument_2);
+            break;
+
+            }
+        prvni_index++;
+        posledni_index--;
+
         }
-    pouzeZnak[index];
-
+        if(jetoPal == 0){
+        printf("%s je to palindrom\n",argument_2);
+        }
     }
-    else if (strcmp(prikaz, treti_Pr)) {
-        for(int i =
-    }
-
+return 0;
 
 
 }
