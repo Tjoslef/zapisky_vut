cb4e54d1570308b4e28640b34e3bfafab052c531
Tjoslef <josef.pasek17@gmail.com>
2025-03-13 12:49:23 +0100

    part a finished

--- IJC/eratosthenes.c
+++ IJC/eratosthenes.c
@@ -1,8 +1,11 @@
 #include "bitset.h"
 #include "error.h"
 void Eratosthenes(bitset_t jmeno_pole){
-   size_t size = bitset_size(jmeno_pole);
-   for (size_t i = 2; i * i <= size; i++) {
+    bitset_fill(jmeno_pole, 1);
+    bitset_setbit(jmeno_pole, 0,0);
+    bitset_setbit(jmeno_pole, 1,0);
+    size_t size = bitset_size(jmeno_pole);
+    for (size_t i = 2; i * i <= size; i++) {
         if (bitset_getbit(jmeno_pole, i) == 1) {
             for (size_t j = i * i; j < size; j += i) {
                 bitset_setbit(jmeno_pole, j, 0);
@@ -10,22 +13,4 @@ void Eratosthenes(bitset_t jmeno_pole){
             }
         }
     }
-    size_t count = 0;
-    for (size_t i = ((jmeno_pole[0] - 1) / 64); i > 0; i--) {
-        for (int j = (int)(sizeof(unsigned long) * 8 - 1); j >= 0; j--){
-            if((jmeno_pole[i] & (1UL << j)) != 0){
-                size_t index = (i - 1) * 64 + j;
-                printf(" %lu \n", index);
-                count++;
-                if (count == 10) {
-                    break; // Break inner loop
-                }
-            }
-        }
-        if (count == 10) {
-            break; // Break outer loop
-        }
-        putchar('\n');
     }
-
-}
--- IJC/steg-decode.c
+++ IJC/steg-decode.c
@@ -14,19 +14,7 @@ int main(int argc,char *argv[]){
     size_t size = my_struct->xsize *my_struct->ysize * 3;
     bitset_alloc(image_bit, size);
     image_bit[0] = size;
-     bitset_fill(image_bit, 1);
-
-    bitset_setbit(image_bit, 0,0);
-    bitset_setbit(image_bit, 1,0);
-   for (size_t i = 2; i * i <= size; i++) {
-        if (bitset_getbit(image_bit, i) == 1) {
-            for (size_t j = i * i; j < size; j += i) {
-                bitset_setbit(image_bit, j, 0);
-
-            }
-        }
-    }
-    int index_bit = 0;
+    Eratosthenes(image_bit);
     size_t index_byte = 0;
     int count = 0;
 char *message = calloc(size, sizeof(char));
@@ -37,34 +25,45 @@ char *message = calloc(size, sizeof(char));
         if((bitset_getbit(image_bit, i)) == 1){
             printf(" %lu ",i);
             int extracted_bit = my_struct->data[i] & 1;
+            int bit_position = 7 - count % 8;
             if (extracted_bit) {
-                       message[index_byte] |= (1U << index_bit);
-                   } else {
-                       message[index_byte] &= ~(1U << index_bit);
+               message[index_byte] |= (1U << bit_position);
+            } else {
+               message[index_byte] &= ~(1U << bit_position);
             }
             count++;
             if(count % 8 == 0){
+                if(message[index_byte] == '\0'){
+                printf("bit-> %c  \n",message[index_byte]);
                 index_byte++;
+                    break;
+                }
                 printf("%c \n",message[index_byte]);
+                index_byte++;
             }
         }
     }
     if(index_byte > 0){
         index_byte++;
     }
-    printf(" number primes in there %c",index_bit);
-    char *new_message = realloc(message, index_byte);
+    printf(" number primes in there %c \n",count);
+    if(message ==NULL){
+        error_exit("something goes wrong in saving");
+    }
+   char *new_message = realloc(message, index_byte);
     if(new_message == NULL){
         error_exit("wrong realloc");
     }else {
         message = new_message;
     }
-size_t p;
+
+//size_t p;
    printf("used bytes %lu",index_byte);
-    for(p = 0;message[p] != '\0';p++){
+  /*  for(p = 0;message[p] != '\0';p++){
         printf("%c ",message[p]);
     }
-printf("number of character in message %lu",p);
+    */
+//printf("number of character in message %lu",p);
     free(message);
     bitset_free(image_bit);
     ppm_free(my_struct);
